ccf4590604d8a3c53e8b8d32db299792adbaf72c
shenyaoguan <chengpengyu_syg@foxmail.com>
2023-09-06 18:40:19 +0800

    对于致谢页面的demo

---
+++ src/components/background.vue
@@ -0,0 +1,15 @@
+<script>
+import {defineComponent} from 'vue'
+
+export default defineComponent({
+  name: "background"
+})
+</script>
+
+<template>
+hello vue！
+</template>
+
+<style scoped>
+
+</style>
---
+++ src/thanks.js
@@ -0,0 +1,5 @@
+import { createApp } from 'vue'
+// 从一个单文件组件中导入根组件
+import App from './thanks.vue'
+
+const app = createApp(App)
